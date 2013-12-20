4a3db48bfbdbb9682b8bfd22cd4678d3310f3160
Fern Wood <wood.fern.l@gmail.com>
2013-12-20 15:54:52 -0800

    Add more tests to goals feature

--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -20,13 +20,49 @@ describe "CRUDing goals" do
     expect(page).to have_content("PRIVATESTUFF")
   end
 
+  it "allows you to create new goals for yourself" do
+    fill_in "goal_name", :with => "lie less"
+    click_on "Add Goal"
+    guest_visit
+    expect(page).to have_conten("lie less")
+  end
+
+  it "allows you to create private goals" do
+    fill_in "goal_name", :with => "more private stuff"
+    check('Private')
+    click_on "Add Goal"
+    expect(page).to have_content("more private stuff")
+  end
+
   it "doesn't show private goals to other users" do
+    fill_in "goal_name", :with => "more private stuff"
+    check('Private')
+    click_on "Add Goal"
+    guest_visit
+    expect(page).to_not have_content("more private stuff")
+  end
+
+  it "allows you to change goals from public to private" do
+    click_on "Make Private"
+    guest_visit
+    expect(page).to_not have_content("my_goal")
+  end
+
+  it "allows you to change goals from private to public" do
+    click_on "Make Public"
+    guest_visit
+    expect(page).to have_content("PRIVATESTUFF")
+  end
+
+  it "allows you to mark a goal as completed" do
+
+  end
+
+  def guest_visit
     user2 = FactoryGirl.build(:user)
     click_on "Sign Out Dawg"
     sign_in(user2)
     visit user_url(user)
-    expect(page).to_not have_content("PRIVATESTUFF")
   end
-
 
 end
